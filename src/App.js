7704908c735705df1f02e71408061721d8054fda
DevEsterCarvalho <estercarvalho205@gmail.com>
2025-04-24 12:01:08 -0300

    estruturando projeto

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import NewProjetc from "./components/pages/NewProject";
+import Container from "./components/layout/Container";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+			<div>
+				<Link to="/">Home</Link>
+				<Link to="/company">Empresa</Link>
+				<Link to="/contact">Contato</Link>
+				<Link to="/newproject">Novo Projeto</Link>
+			</div>
+			<Container customClass="min-height">
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/company" element={<Company />} />
+					<Route path="/contact" element={<Contact />} />
+					<Route path="/newproject" element={<NewProjetc />} />
+				</Routes>
+			</Container>
+			<p>Footer</p>
+		</Router>
+	);
 }
 
 export default App;
---
+++ src/components/pages/NewProject.js
@@ -0,0 +1,4 @@
+function NewProjetc() {
+	return <h1>NewProjetc</h1>;
+}
+export default NewProjetc;
